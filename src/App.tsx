c699bc63e24df4716d26a5244997a14c19c06857
Aswinpranav <ravipranav2005@gmail.com>
2026-01-11 13:24:19 +0530

    Fix Netlify white page issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
+import type { DataTablePageEvent } from "primereact/datatable";
 import { Column } from "primereact/column";
 import type { Artwork } from "./types";
 
@@ -9,17 +10,15 @@ function App() {
   const [totalRecords, setTotalRecords] = useState(0);
   const [loading, setLoading] = useState(false);
 
-  // Store ONLY selected row IDs (important rule)
+  // store ONLY selected row IDs
   const [selectedIds, setSelectedIds] = useState<Set<number>>(new Set());
 
-  // Fetch ONLY current page data
   const fetchArtworks = async (pageNumber: number) => {
     setLoading(true);
 
     const response = await fetch(
       `https://api.artic.edu/api/v1/artworks?page=${pageNumber}&limit=10`
     );
-
     const result = await response.json();
 
     setRows(result.data);
@@ -31,13 +30,12 @@ function App() {
     fetchArtworks(page);
   }, [page]);
 
-  // Map selected IDs → rows of CURRENT page only
+  // map selected IDs to current page rows only
   const selectedRows = rows.filter((row) => selectedIds.has(row.id));
 
   return (
     <div style={{ padding: "20px" }}>
       <h2>Art Institute of Chicago – Artworks</h2>
-      <p>Selected artworks: <strong>{selectedIds.size}</strong></p>
 
       <DataTable
         value={rows}
@@ -47,26 +45,27 @@ function App() {
         rows={10}
         totalRecords={totalRecords}
         loading={loading}
-        emptyMessage="No artworks found"
+        selectionMode="multiple"
         selection={selectedRows}
         onSelectionChange={(e) => {
           const newSet = new Set<number>();
+          const value = e.value as Artwork[] | null;
 
-          (e.value as Artwork[]).forEach((row) => {
-            newSet.add(row.id);
-          });
-
+          value?.forEach((row) => newSet.add(row.id));
           setSelectedIds(newSet);
         }}
-        onPage={(e) => setPage(e.page + 1)}
+        onPage={(e: DataTablePageEvent) => {
+          if (typeof e.page === "number") {
+            setPage(e.page + 1);
+          }
+        }}
       >
-        <Column selectionMode="multiple" headerStyle={{ width: "3rem" }} />
+        <Column selectionMode="multiple" style={{ width: "3rem" }} />
         <Column field="title" header="Title" />
         <Column field="place_of_origin" header="Place of Origin" />
         <Column field="artist_display" header="Artist" />
         <Column field="inscriptions" header="Inscriptions" />
         <Column field="date_start" header="Start Date" />
-        <Column field="inscriptions" header="Inscriptions"style={{ maxWidth: "300px", whiteSpace: "normal" }}/>
         <Column field="date_end" header="End Date" />
       </DataTable>
     </div>
